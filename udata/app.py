b2af8b8842789911eb71bf34eb8ad2869227c194
Adriano Regis <adbrum@outlook.com>
2026-03-23 11:48:35 +0000

    chore: increase default rate limits for the application.

--- udata/app.py
+++ udata/app.py
@@ -39,7 +39,7 @@ cache = Cache()
 csrf = CSRFProtect()
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["10000 per day", "5000 per hour"],
     storage_uri="memory://",
 )
 
